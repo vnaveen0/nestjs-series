fc22b9d0f319f69a9e85eabb27f61b3b14cf3bfc
Naveen Vedula <vnaveen0@gmail.com>
2024-06-01 17:58:55 -0700

    Error Handling

--- lesson01/src/users/users.service.ts
+++ lesson01/src/users/users.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
+
 
 @Injectable()
 export class UsersService {
@@ -40,13 +42,18 @@ export class UsersService {
 
     findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
         if (role) {
-            return this.users.filter(user => user.role === role)
+            const rolesArray = this.users.filter(user => user.role === role)
+            if (rolesArray.length === 0) throw new NotFoundException('User Role not found!')
+            return rolesArray
         }
         return this.users
     }
 
     findOne(id: number) {
-        return this.users.find(user => user.id === id);
+        const user = this.users.find(user => user.id === id);
+        if (!user) throw new NotFoundException('User not found!')
+        
+        return user
     }
 
 
--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
-a
+https://www.youtube.com/watch?v=8_X0nSrzrCw&t=5403s
+Course Done here
