408174bcdc46119e77cee141d9ce1b3d98ba1b01
Naveen Vedula <vnaveen0@gmail.com>
2024-06-01 17:02:16 -0700

    adding users

---
+++ lesson01/src/users/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from "class-validator";
+export class CreateUserDto {
+
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsEnum(["INTERN",  "ENGINEER", "ADMIN"],
+        { message: 'Valid Role required' })
+    role: "INTERN" | "ENGINEER" | "ADMIN";
+}
+
+
--- lesson01/src/users/users.service.ts
+++ lesson01/src/users/users.service.ts
@@ -1,4 +1,80 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id": 1,
+            "name": "Leanne Graham",
+            "email": "Sincere@april.biz",
+            "role": "INTERN",
+        },
+        {
+            "id": 2,
+            "name": "Ervin Howell",
+            "email": "Shanna@melissa.tv",
+            "role": "INTERN",
+        },
+        {
+            "id": 3,
+            "name": "Clementine Bauch",
+            "email": "Nathan@yesenia.net",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 4,
+            "name": "Patricia Lebsack",
+            "email": "Julianne.OConner@kory.org",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 5,
+            "name": "Chelsey Dietrich",
+            "email": "Lucio_Hettinger@annie.ca",
+            "role": "ADMIN",
+        }
+    ];
+
+
+    findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        if (role) {
+            return this.users.filter(user => user.role === role)
+        }
+        return this.users
+    }
+
+    findOne(id: number) {
+        return this.users.find(user => user.id === id);
+    }
+
+
+    create(createUserDto: CreateUserDto) {
+        const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id)
+        const newUser = {
+            id: usersByHighestId[0].id + 1,
+            ...createUserDto
+        }
+        this.users.push(newUser)
+        return newUser
+    }
+
+    update(id: number, updateUserDto: UpdateUserDto) {
+        this.users = this.users.map(user => {
+            if (user.id === id) {
+                return { ...user, ...updateUserDto }
+            }
+            return user
+        })
+
+        return this.findOne(id)
+    }
+
+    delete(id: number) {
+        const removedUser = this.findOne(id)
+        this.users.filter(user => user.id !== id)
+        return removedUser
+    }
+
+}
